cfb6554520c9c82d10fc2c85e16afdfe04f26c00
Domenico Rizzo <dev@idsign.it>
2017-07-04 12:43:22 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<templatex>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</templatex>
+
+<template>
+  <main-app></main-app>
+</template>
+
+<script>
+  import MainApp from './components/Main.vue';
+  export default {
+    name: 'app',
+    components: {
+      MainApp
+    }
+  };
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
